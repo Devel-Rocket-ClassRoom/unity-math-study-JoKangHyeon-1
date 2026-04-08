b6b1376f6fa308f21eab508d3d6f54525e63337f
PlatinaCode <jokh980324@naver.com>
2026-04-08 13:13:27 +0900

    제출2

--- Assets/GameMathCurriculum/Ch06/Scripts/Assignment_PatrolGuard.cs
+++ Assets/GameMathCurriculum/Ch06/Scripts/Assignment_PatrolGuard.cs
@@ -41,7 +41,8 @@ public class Assignment_PatrolGuard : MonoBehaviour
 
     private void Start()
     {
-        // TODO
+        transform.rotation = Quaternion.Euler(0f, startYAngle, 0f);
+        currentWaypointIndex = 0;
     }
 
     private void Update()
@@ -54,9 +55,9 @@ public class Assignment_PatrolGuard : MonoBehaviour
         dirToNext.Normalize();
         distanceToNext = Vector3.Distance(transform.position, target.position);
 
-        // TODO
-
-        // TODO
+        Quaternion deltaRotation = Quaternion.FromToRotation(transform.forward, dirToNext);
+        targetRotation = transform.rotation * deltaRotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
 
         // 이동 (제공됨 — 수정 불필요)
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
